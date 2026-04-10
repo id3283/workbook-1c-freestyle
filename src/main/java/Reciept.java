caf9f44f3c9ef36e4323c0b0bca0aed3b00b0d62
Dave Dykes <148535937+brightworkdigital@users.noreply.github.com>
2026-04-10 14:29:34 +0100

    Added console input stuff.

---
+++ src/main/java/Reciept.java
@@ -0,0 +1,10 @@
+public class Reciept {
+
+	public static void main(String[] args) {
+		String itemName = "item";
+		double itemPrice = 0.99;
+		int quantity = 3;
+
+		System.out.println("Sucker, you bought " + quantity + " " + itemName +"s for a total of :" + quantity * itemPrice);
+	}
+}
